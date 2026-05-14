ad015c092f942de476b47767407838ab9502b5b3
admmarcelac-ai <adm.marcelac@gmail.com>
2026-05-14 15:56:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+import re
 
-st.title("PDF a Excel - Test")
+st.title("PDF a Excel")
 
 archivo = st.file_uploader("Subí un PDF", type="pdf")
 
@@ -9,6 +10,10 @@ if archivo:
     reader = PdfReader(archivo)
     texto = reader.pages[0].extract_text()
 
-    st.success("PDF leído correctamente")
-
     st.text_area("Texto detectado", texto, height=300)
+
+    lineas = texto.split("\n")
+
+    for linea in lineas:
+        if "unidades" in linea:
+            st.write("Detectado:", linea)
